3055a1db434a9fecc6b66096e9189a6731b3c8de
MateusSantoss <61834160+MateusSantoss@users.noreply.github.com>
2020-03-05 10:38:03 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	int base;
+	int altura;
+	
+	printf ("digite a base do retangulo\n");
+	
+	scanf ("%d" ,&base);
+	
+	printf ("digite a altura do retangulo\n");
+	
+	scanf ("%d" ,&altura);
+	
+	printf("sua area e %d", base * altura);
+	
+	
+	
+	return 0;
+}
